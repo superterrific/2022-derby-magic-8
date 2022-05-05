223b28fe356ef2ed03d2ce2169d89c53d6620860
Dana Byerly <dana@superterrific.org>
2022-05-05 14:50:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# 2022 Kentucky Derby Magic 8 Ball
+
+Going to a Derby party and need someone to root for? Looking to blow some money on the ponies but don't know anything about it?
+
+The [Kentucky Derby Magic 8 Ball](link) can help!
+
+This year's Kentucky Derby is scheduled for Saturday May 7, 2022.
+
+---
+
+## A brief history...
+
+In 2018 I created a [2018 Kentucky Derby Magic 8 Ball](https://2018-derby-8ball.glitch.me/) based on [this tutorial by Kelly Lougheed](https://medium.com/@kellylougheed/javascript-magic-8-ball-with-basic-dom-manipulation-1636b83c3c26) and by remixing her [Glitch Project](https://glitch.com/~8ball-starter) (thank you!!).
+
+Since then I've tried to improve it a bit each year. The full history can be found on the project page [at my website](https://danabyerly.com/projects/kentucky-derby-magic-8-ball/).
---
+++ script.js
@@ -0,0 +1,38 @@
+const contenders = [
+  "Epicenter",
+  "Zandon",
+  "White Abarrio",
+  "Mo Donegal",
+  "Tiz the Bomb",
+  "Cyberknife",
+  "Crown Pride (Jpn)",
+  "Taiba",
+  "Simlification",
+  "Smile Happy",
+  "Classic Causeway",
+  "Tawny Port",
+  "Barber Road",
+  "Messier",
+  "Zozos",
+  "Summer Is Tomorrow",
+  "Charge It",
+  "Happy Jack",
+  "Pioneer of Medina",
+  "In Due Time",
+  "Ethereal Road",
+  "No one on your tickets!"
+];
+
+document.getElementById('answer-button').onclick = function () {
+
+  document.getElementById('answer-container').classList.remove('eight');
+  document.getElementById('answer-container').classList.add('reveal');
+
+  // remove aria-hidden attribute so the answer is read
+  document.getElementById('answer-container').setAttribute("aria-hidden", false);
+  document.getElementById('answer').classList.add('answer');
+  document.getElementById('eight-ball').classList.add('shake');
+  let derbyWinner = contenders[Math.floor(Math.random() * contenders.length)];
+
+  document.getElementById('answer-container').textContent = derbyWinner;
+};
